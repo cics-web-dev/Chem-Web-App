734eb6cd99b11d9c95cca48bb1fa3f6abbd97d8d
Shamanth-GiT <skuthpadi@umass.edu>
2024-02-22 17:10:27 -0500

    Changes to router

--- backend/src/api/services/question/question.routes.ts
+++ backend/src/api/services/question/question.routes.ts
@@ -7,5 +7,9 @@ const router = Router();
 router.get('/:id', questionController.getSingleQuestion);
 router.get('/sidebar/list', questionController.getSidebarQuestions);
 router.post('/bookmark/:studentid/:bookmarkid', questionController.bookmarkQuestion);
-
+// update the status of the question for a student
+// change incomplete to complete or vice versa
+router.post('/status/:studentid/:id', questionController.updateQuestionStatus);
+// update previous progress of the question by id
+router.post('/:studentid/:id', questionController.updateQuestionProgress);
 export default router;
