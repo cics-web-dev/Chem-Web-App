a2b39e17764770c8d7a57f51cbd157d36332470e
ShiraBen-Anat <sbenanat@umass.edu>
2024-02-25 09:55:24 -0500

    fixed radio button to checkbox, multiple correct answers

--- frontend/src/lib/components/QuestionOptions.svelte
+++ frontend/src/lib/components/QuestionOptions.svelte
@@ -1,17 +1,24 @@
 <script>
+    import _ from 'lodash';
     export let question;
-    let answer = null;
+    let answer = [];
     let show_feedback = false;
 
     function check_answer(event) {
-        return answer === question.correct ? console.log("right") : show_feedback = true;
+        console.log(answer);
+        return _.isEmpty(_.xor(question.correct, answer)) ? console.log("right") : show_feedback = true;
     }  
 </script>
  <!-- We can add different types of question options 
     (like matching questions or fill in the blank) here in the future -->
 <form on:submit={check_answer}>
+    
     {#each question.options as option, i}
-        <input type="radio" id={i} bind:group={answer} value={i}>
+        {#if question.correct.length > 1}
+            <input type="checkbox" id={i} bind:group={answer} value={i}>
+        {:else}
+            <input type="radio" id={i} bind:group={answer} value={i}>
+        {/if}
         <label for={i}>{option}</label>
         <br>
     {/each}
