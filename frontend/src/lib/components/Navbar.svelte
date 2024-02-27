3b2f7d2f5b15e3d2b1f47910d6779b3ba0b75e92
ShiraBen-Anat <sbenanat@umass.edu>
2024-02-27 18:41:40 -0500

    simple navbar directs to login and signup and back to question

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -5,4 +5,13 @@
     */
 </script>
 
-<h1>NAVBAR</h1>
+<header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+    <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+      <a class="flex-none text-xl font-semibold dark:text-white" href="/">Home</a>
+      <div class="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
+        <a class="font-medium  hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/login" aria-current="page">Log In</a>
+        <a class="font-medium hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/signup">Sign Up</a>
+
+      </div>
+    </nav>
+  </header>
