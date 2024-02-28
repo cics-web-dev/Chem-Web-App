b73594e3f1c9c8ba5a76541ea060f55746f9d0c6
Yongye Tan <yongyetan1209@gmail.com>
2024-02-28 12:42:33 -0500

    Separate Navbar and collapse button and apply a new preprocesser to support postcss

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
     import '../app.pcss';
-    import Navbar from '$components/Navbar.svelte';
+    import Navbar from '$components/NavBar/Navbar.svelte';
 </script>
 
 <Navbar />
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,24 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
 import path from 'path';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
-    preprocess: [vitePreprocess({})],
+    preprocess: [
+        vitePreprocess({}),
+        preprocess({
+            postcss: {
+                configFilePath: join(__dirname, 'postcss.config.js')
+            }
+        })
+    ],
 
     kit: {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
