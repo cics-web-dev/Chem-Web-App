0cee26cf4a8a94fad1c1e2348e13c7319aa43bae
Yongye Tan <yongyetan1209@gmail.com>
2024-03-12 12:35:28 -0400

    Add completion status

--- frontend/src/lib/components/Sidebar/SidebarQuestionList.svelte
+++ frontend/src/lib/components/Sidebar/SidebarQuestionList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import Beaker from '$icons/Sidebar/Beaker.svelte';
+    import StatusIcon from '$icons/Sidebar/CompletionStatus.svelte';
     export let questions: any;
 </script>
 
@@ -11,11 +11,11 @@
         {#each questions as question}
             <li class="pb-px">
                 <a class="question-list-item-style" href="/questions/{question.id}">
-                    <Beaker />
+                    <StatusIcon isCompleted={question.isCompleted} />
                     {question.name}
                 </a>
                 <!-- will need to find a bookmark icon -->
-                <button on:click={() => console.log('A')}>bookmark</button>
+                <!-- <button on:click={() => console.log('A')}>bookmark</button> -->
             </li>
         {/each}
     </ul>
@@ -25,13 +25,13 @@
     .question-list-item-style {
         @apply flex items-center gap-x-4 rounded-lg px-3 py-2 text-sm text-slate-700 hover:bg-gray-100 dark:bg-gray-800 dark:text-slate-400 dark:hover:bg-gray-900 dark:hover:text-slate-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600;
     }
-    
-    li {
-        @apply flex items-center justify-start gap-x-3.5;
-    }
 
-    button {
+    /* li {
+        @apply flex items-center justify-start gap-x-3.5;
+    } */
+
+    /* button {
         margin-left: auto;
         @apply rounded-lg px-2.5 py-1 text-sm text-slate-700 hover:bg-gray-100 dark:bg-gray-800 dark:text-slate-400 dark:hover:bg-gray-900 dark:hover:text-slate-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600;
-    }
+    } */
 </style>
---
+++ frontend/src/lib/icons/Sidebar/CompletionStatus.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    // to indicate whether a question is completed or not
+    export let isCompleted: boolean;
+</script>
+
+{#if isCompleted}
+    <!-- Checkmark icon when question is completed (need to add color) -->
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-check"
+        ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 12l5 5l10 -10" /></svg
+    >
+{:else}
+    <!-- Full circle icon when the question is not completed  -->
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-circle"
+        ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+            d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"
+        /></svg
+    >
+{/if}
