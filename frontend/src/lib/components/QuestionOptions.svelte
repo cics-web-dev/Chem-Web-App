a9219bbddb4c67f238478829f245f31e6c1acde5
ShiraBen-Anat <sbenanat@umass.edu>
2024-03-14 12:09:01 -0400

    fixed formatting on smaller screens

--- frontend/src/lib/components/QuestionOptions.svelte
+++ frontend/src/lib/components/QuestionOptions.svelte
@@ -33,55 +33,70 @@
 
 <!-- We can add different types of question options 
     (like matching questions or fill in the blank) here in the future -->
+<div class="mx-auto  px-4 py-4 sm:px-6 lg:px-8">
 
-<form on:submit={check_answer}>
-    {#each question.options as option, index}
-        <!-- this div is displayed if the form has been submitted and the answer is correct -->
-        {#if correct.includes(index)}
-            <div class="m-3 w-1/2 rounded-lg bg-green-200 p-3">
-                <DisplayOption {option} {index} correct={question.correctAnswers} checked={false} />
+    <div class="grid justify-center gap-3 md:grid-cols-2 md:items-center md:justify-between">
+        <form on:submit={check_answer}>
+            {#each question.options as option, index}
+                <!-- this div is displayed if the form has been submitted and the answer is correct -->
+                {#if correct.includes(index)}
+                    <div class="m-3 rounded-lg bg-green-200 p-3">
+                        <DisplayOption {option} {index} correct={question.correctAnswers} checked={false} />
+                    </div>
+
+                    <!-- this div is displayed if the form has been submitted and the answer is incorrect -->
+                {:else if incorrect.includes(index)}
+                    <div class="m-3 rounded-lg bg-red-200 p-3">
+                        <DisplayOption {option} {index} correct={question.correctAnswers} checked={false} />
+                    </div>
+
+                    <!-- this div is displayed if the form has not been submitted and the option is selected -->
+                {:else if selected_answer.includes(index)}
+                    <div class="m-3 rounded-lg bg-slate-300 p-3">
+                        <DisplayOption {option} {index} correct={question.correctAnswers} checked={true} />
+                    </div>
+
+                    <!-- this div is displayed if the form has not been submitted and the option is not selected yet -->
+                {:else}
+                    <div class="m-3 rounded-lg p-3 hover:bg-slate-300">
+                        <DisplayOption {option} {index} correct={question.correctAnswers} checked={false} />
+                    </div>
+                {/if}
+            {/each}
+
+            <div class="flex justify-center items-center">
+                <!-- this is the submit button -->
+                <button class="navigation-button-style" type="submit">Submit</button> 
             </div>
+            
+            
+            <div class="flex justify-center items-center">
+                <!-- this is the back button -->
+                <button class="navigation-button-style">
+                    <BackArrowIcon />
+                </button>
 
-            <!-- this div is displayed if the form has been submitted and the answer is incorrect -->
-        {:else if incorrect.includes(index)}
-            <div class="m-3 w-1/2 rounded-lg bg-red-200 p-3">
-                <DisplayOption {option} {index} correct={question.correctAnswers} checked={false} />
+                <!-- this is the next button -->
+                <button class="navigation-button-style">
+                    <NextArrowIcon />
+                </button>  
             </div>
+          
+            
+        </form>
 
-            <!-- this div is displayed if the form has not been submitted and the option is selected -->
-        {:else if selected_answer.includes(index)}
+        <!-- This will display the feedback display -->
+        {#if show_feedback}
             <div class="m-3 w-1/2 rounded-lg bg-slate-300 p-3">
-                <DisplayOption {option} {index} correct={question.correctAnswers} checked={true} />
+                <p>{question.feedback}</p>
             </div>
-
-            <!-- this div is displayed if the form has not been submitted and the option is not selected yet -->
         {:else}
-            <div class="m-3 w-1/2 rounded-lg p-3 hover:bg-slate-300">
-                <DisplayOption {option} {index} correct={question.correctAnswers} checked={false} />
-            </div>
+            <p style="display:none">{question.feedback}</p>
         {/if}
-    {/each}
 
-    <!-- this is the submit button -->
-    <button class="navigation-button-style" type="submit">Submit</button>
-    <br />
-
-    <!-- this is the back button -->
-    <button class="navigation-button-style">
-        <BackArrowIcon />
-    </button>
-
-    <!-- this is the next button -->
-    <button class="navigation-button-style">
-        <NextArrowIcon />
-    </button>
-</form>
-
-<!-- This will display the feedback display -->
-{#if show_feedback}
-    <div class="m-3 w-1/2 rounded-lg bg-slate-300 p-3">
-        <p>{question.feedback}</p>
     </div>
-{:else}
-    <p style="display:none">{question.feedback}</p>
-{/if}
+    
+
+</div>
+
+
