29828c584554b5c6b9fb1cabf79064eee45a318f
Yongye Tan <yongyetan1209@gmail.com>
2024-03-14 22:51:51 -0400

    change to full filled circle

--- frontend/src/lib/icons/Sidebar/CompletionStatus.svelte
+++ frontend/src/lib/icons/Sidebar/CompletionStatus.svelte
@@ -3,36 +3,43 @@
     export let isCompleted: boolean;
 </script>
 
+<!-- Credit: https://tablericons.com/ -->
 {#if isCompleted}
     <!-- Checkmark icon when question is completed (need to add color) -->
     <svg
         xmlns="http://www.w3.org/2000/svg"
+        class="icon icon-tabler icon-tabler-check"
         width="20"
         height="20"
         viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
         stroke-width="1.5"
+        stroke="#00b341"
+        fill="none"
         stroke-linecap="round"
         stroke-linejoin="round"
-        class="icon icon-tabler icons-tabler-outline icon-tabler-check"
-        ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 12l5 5l10 -10" /></svg
     >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M5 12l5 5l10 -10" />
+    </svg>
 {:else}
     <!-- Full circle icon when the question is not completed  -->
     <svg
         xmlns="http://www.w3.org/2000/svg"
+        class="icon icon-tabler icon-tabler-circle-filled"
         width="20"
         height="20"
         viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
         stroke-width="1.5"
+        stroke="#00b341"
+        fill="none"
         stroke-linecap="round"
         stroke-linejoin="round"
-        class="icon icon-tabler icons-tabler-outline icon-tabler-circle"
-        ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
-            d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"
-        /></svg
     >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path
+            d="M7 3.34a10 10 0 1 1 -4.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 4.995 -8.336z"
+            stroke-width="0"
+            fill="currentColor"
+        />
+    </svg>
 {/if}
