de9ce164564174eb443d0d38166dbcddb9749552
Yongye Tan <yongyetan1209@gmail.com>
2024-03-31 23:47:21 -0400

    fix question import

--- frontend/src/routes/questions/[slug]/+page.svelte
+++ frontend/src/routes/questions/[slug]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import DisplayQuestion from '$components/DisplayQuestion.svelte';
+    import DisplayQuestion from '$components/Questions/DisplayQuestion.svelte';
     import { isSidebarOpenStore } from '$stores/SidebarOpenStore';
 
     let isSidebarOpen: boolean;
