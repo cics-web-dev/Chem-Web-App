760421acc0c383b4692d2782308300411a4e56e1
Yongye Tan <yongyetan1209@gmail.com>
2024-04-03 15:43:00 -0400

    add mongo db url and instructor

--- backend/src/api/configs/mongoose.config.ts
+++ backend/src/api/configs/mongoose.config.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { set } from 'mongoose';
 
 
 /**
@@ -19,15 +19,27 @@ export class MongoDB {
     /**
      * The MongoDB URL to connect to.
      */
-    private static MONGODB_URL = 'mongodb://localhost:27017/ChemWebApp';
+    private static MONGODB_URL = '';
+
+    /**
+     * Sets the MongoDB URL.
+     */
+    private static setMongoDBURL() {
+        if (process.env.MONGODB_URL) {
+            MongoDB.MONGODB_URL = process.env.MONGODB_URL;
+        } else {
+            throw new Error('MongoDB URL is not found in the .env file');
+        }
+    }
 
     /**
      * Establishes a connection to MongoDB using mooogose's `connect` method.
      */
     public static async connect() {
         try {
+            MongoDB.setMongoDBURL();
             await mongoose.connect(MongoDB.MONGODB_URL);
-            console.log('Pinged! You successfully connected to MongoDB!');
+            console.log('Pinged! You successfully connected to MongoDB Altas!');
         } catch (error) {
             console.error(error);
         }
