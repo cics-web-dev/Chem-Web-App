616532bd5c2238eea0b46bc3cd10b34484d7de23
DarzyD <dangbrian96@gmail.com>
2024-04-10 18:18:06 -0400

    Added difficulty tags based on question data

---
+++ frontend/src/lib/components/Questions/Difficulty.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import '$styles/Difficulty.pcss';
+    export let difficulty: string;
+</script>
+
+{#if difficulty == "Easy"}
+    <span class="easy">Easy</span>
+{:else if difficulty == "Medium"}
+    <span class="medium">Medium</span>
+{:else if difficulty == "Hard"}
+    <span class="hard">Hard</span>
+{/if}
