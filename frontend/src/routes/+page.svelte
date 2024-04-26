f71c2fdc2d0a2b2d3a828d161e306194aca65884
Bao Dang <baochidangg@gmail.com>
2024-04-26 14:40:20 -0400

    Main page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>This is where the main page is shown</h1>
-<h2>Hoverable tiles</h2>
+<script lang="ts">
+    const numberOfParticles = Math.random() * 100;
+    const particles = Array.from({ length: numberOfParticles }, (_, index) => index);
+</script>
+
+<h1>SkoutaLab</h1>
+<p>Organic Chemistry, but now more fun and interactive!</p>
+<button>Start learning</button>
